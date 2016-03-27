414db22a0a105bff8c2c87a3e91292ee20f64f86
Keivan Beigi <me@keivan.io>
2016-03-26 23:45:12 -0700

    Fixed calendar in M3

--- frontend/src/Handlebars/Helpers/DateTime.js
+++ frontend/src/Handlebars/Helpers/DateTime.js
@@ -6,7 +6,6 @@ var day = require('./DateTime/day');
 var month = require('./DateTime/month');
 var lts = require('./DateTime/lts');
 var formatDate = require('./DateTime/formatDate');
-var ifToday = require('./DateTime/ifToday');
 var unlessToday = require('./DateTime/unlessToday');
 
 Handlebars.registerHelper('shortDate', shortDate);
@@ -16,5 +15,4 @@ Handlebars.registerHelper('month', month);
 Handlebars.registerHelper('StartTime', startTime);
 Handlebars.registerHelper('lts', lts);
 Handlebars.registerHelper('formatDate', formatDate);
-Handlebars.registerHelper('if_today', ifToday);
 Handlebars.registerHelper('unless_today', unlessToday);
