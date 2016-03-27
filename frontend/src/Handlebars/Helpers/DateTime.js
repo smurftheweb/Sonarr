0979dc11edbf85542807ca79ed9d04317cd22c2e
Keivan Beigi <me@keivan.io>
2016-03-26 23:45:21 -0700

    removed unlessToday

--- frontend/src/Handlebars/Helpers/DateTime.js
+++ frontend/src/Handlebars/Helpers/DateTime.js
@@ -6,7 +6,6 @@ var day = require('./DateTime/day');
 var month = require('./DateTime/month');
 var lts = require('./DateTime/lts');
 var formatDate = require('./DateTime/formatDate');
-var unlessToday = require('./DateTime/unlessToday');
 
 Handlebars.registerHelper('shortDate', shortDate);
 Handlebars.registerHelper('RelativeDate', relativeDate);
@@ -15,4 +14,3 @@ Handlebars.registerHelper('month', month);
 Handlebars.registerHelper('StartTime', startTime);
 Handlebars.registerHelper('lts', lts);
 Handlebars.registerHelper('formatDate', formatDate);
-Handlebars.registerHelper('unless_today', unlessToday);
